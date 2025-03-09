1cea99060b93b9de95729ef6ed69a27b6b3d1096
Balaramansethu <baalaaraman@gmail.com>
2025-03-09 09:17:02 +0530

    feat - implemented routes for redirecting to different pages

--- src/components/HomeComponent/HomeComponent.jsx
+++ src/components/HomeComponent/HomeComponent.jsx
@@ -1,27 +1,41 @@
-import React from "react";
-import "./HomeComponent.css"; 
-import ContentComponent from "../ContentComponent/ContentComponent";
+import React from "react"
+import "./HomeComponent.css"
+import { useEffect, useState } from "react"
+import ContentComponent from "../ContentComponent/ContentComponent"
+import axios from "axios"
 
-const HomeComponent = (props) => {
-  return (
-    <div className="main-container">
-      <div className='coin-row'>
-        {/* <div className="heading">
-          <p>Name</p>
-          <p className="coin-name">coin</p>
-          <p>Price</p>
-          <p>24h</p>
-          <p className="hide-mobile">Volume</p>
-          <p className="hide-mobile">Market</p>
-        </div> */}
-        {props.value && props.value.map((result, index) => (
-          <ContentComponent key={index} value={result} />
-        ))}
-      </div>
-    </div>
-  );
-};
+const HomeComponent = () => {
+    const [fetchApi, setFetchApi] = useState()
 
+    const handleApi = () => {
+        axios
+            .get(
+                "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr"
+            )
+            .then((response) => {
+                setFetchApi(response.data)
+            })
+            .catch((error) => {
+                {
+                    error
+                }
+            })
+    }
 
+    useEffect(() => {
+        handleApi()
+    }, [])
 
-export default HomeComponent;
+    return (
+        <div className="main-container">
+            <div className="coin-row">
+                {fetchApi &&
+                    fetchApi.map((result, index) => (
+                        <ContentComponent key={index} value={result} />
+                    ))}
+            </div>
+        </div>
+    )
+}
+
+export default HomeComponent
---
+++ src/routes/NavigationComponent/NavigationComponent.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom"
+import HomeComponent from "../../components/HomeComponent/HomeComponent"
+import ContactUsComponent from "../../components/ContactUsComponent/ContactUsComponent"
+import AboutComponent from "../../components/AboutComponent/AboutComponent"
+const NavigationComponent = () => {
+    return (
+        <Router>
+            <div className="heading">
+                <Link to="/">
+                    <p>Home</p>
+                </Link>
+                <Link to="About">
+                    <p>About</p>
+                </Link>
+                <Link to="contact">
+                    <p>Contact us</p>
+                </Link>
+            </div>
+            <Routes>
+                <Route path="/" element={<HomeComponent />} />
+                <Route path="About" element={<AboutComponent />} />
+                <Route path="contact" element={<ContactUsComponent />} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default NavigationComponent
