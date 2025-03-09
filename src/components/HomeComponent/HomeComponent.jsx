6091dc68e5dba04279ac80289c81d01894527c28
Balaramansethu <baalaaraman@gmail.com>
2025-03-09 05:32:34 +0530

    feat - created card components to display the stats

--- src/components/HomeComponent/HomeComponent.jsx
+++ src/components/HomeComponent/HomeComponent.jsx
@@ -1,26 +1,27 @@
-import React from "react"
-import "./HomeComponent.css"
-import ContentComponent from "../ContentComponent/ContentComponent"
+import React from "react";
+import "./HomeComponent.css"; 
+import ContentComponent from "../ContentComponent/ContentComponent";
 
 const HomeComponent = (props) => {
-    return (
-        <div className="main-container">
-            <div>
-                <div className="heading">
-                    <p>Name</p>
-                    <p className="coin-name">coin</p>
-                    <p>Price</p>
-                    <p>24h</p>
-                    <p className="hide-mobile">Volume</p>
-                    <p className="hide-mobile">Market</p>
-                </div>
-                {props.value &&
-                    props.value.map((result, index) => (
-                        <ContentComponent key={index} value={result} />
-                    ))}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="main-container">
+      <div className='coin-row'>
+        {/* <div className="heading">
+          <p>Name</p>
+          <p className="coin-name">coin</p>
+          <p>Price</p>
+          <p>24h</p>
+          <p className="hide-mobile">Volume</p>
+          <p className="hide-mobile">Market</p>
+        </div> */}
+        {props.value && props.value.map((result, index) => (
+          <ContentComponent key={index} value={result} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default HomeComponent
+
+
+export default HomeComponent;
